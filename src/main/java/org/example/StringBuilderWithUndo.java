17e85a0f173e06d0ced9a1eccab67fe76a14e14f
Pavlov-Andrey-V <pavlov.and.v@mail.ru>
2026-06-03 12:35:16 +0300

    Revert "Class is implemented with method undo and pattern snapshot"

--- src/main/java/org/example/StringBuilderWithUndo.java
+++ src/main/java/org/example/StringBuilderWithUndo.java
@@ -1,42 +1,5 @@
 package org.example;
 
-import java.util.Stack;
-
 public class StringBuilderWithUndo {
-    private StringBuilder stringBuilder;
-    private Stack<Snapshot> snapshots;
 
-    public StringBuilderWithUndo() {
-        stringBuilder = new StringBuilder();
-        snapshots = new Stack<>();
-    }
-
-    private static class Snapshot {
-        private final String conditional;
-
-        public Snapshot(String conditional) {
-            this.conditional = conditional;
-        }
-
-        public String getConditional() {
-            return conditional;
-        }
-    }
-
-    private void takeSnapshot() {
-        snapshots.push(new Snapshot(stringBuilder.toString()));
-    }
-
-    public StringBuilderWithUndo append(String someString) {
-        takeSnapshot();
-        stringBuilder.append(someString);
-        return this;
-    }
-
-    public void undo() {
-        if (!snapshots.isEmpty()) {
-            Snapshot lastSnapshot = snapshots.pop();
-            stringBuilder = new StringBuilder(lastSnapshot.getConditional());
-        }
-    }
 }
